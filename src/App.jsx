600bfaf1e7a383a4bb37624136923b6798e281f4
Majid Ash <Magid.Ash.Si@gmail.com>
2023-12-30 07:35:01 -0800

    1- Creating a Crypto App2 project 2- Using test API from website https://api.coingecko.com/api/v3 3- Fetch data from API 4- Create services directory and cryptoApi.js file for API management 5 - showing coins in the table 6- extracting the TableRow components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import HomePage from "./components/templates/HomePage"
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/templates/HomePage.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+import TableCoin from "../modules/TableCoin"
+import {getCoinList} from "../../services/cryptoApi"
+
+function HomePage() {
+
+   const [coins , setCoins] = useState([])
+
+  //  useEffect(() => {
+  //   fetch(getCoinList())
+  //       .then((res) => res.json())
+  //       .then((json) => setCoins(json));
+  //  } ,[])
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch(getCoinList());
+      const json = await res.json();
+      setCoins(json);
+    }
+    getData()
+  } , [])
+
+  return (
+    <div><TableCoin coins={coins} /></div>
+  )
+}
+
+export default HomePage
