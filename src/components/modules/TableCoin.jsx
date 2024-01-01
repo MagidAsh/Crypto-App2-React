c681704a964a79aa5dd9dd3af1abf1d0523b9048
Majid Ash <Magid.Ash.Si@gmail.com>
2024-01-01 03:42:39 -0800

    1 - Adding a Loader to the project using the react.loader.spinner package 2 - stylling the prices table 3 - Implementation of Pagination Component 4 - paginate data of coins 5 - stylling of Pagination Component

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,26 +1,34 @@
+// imgages
 import chartUp from "../../assets/chart-up.svg"
 import chartDown from "../../assets/chart-down.svg"
 
- 
+// component
+import { RotatingLines } from "react-loader-spinner";
 
-function TableCoin({coins}) {
-    console.log(coins);
+// style
+import styles from "./TableCoin.module.css" ;
+
+function TableCoin({coins , isLoading}) {
+
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Coin</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>24h</th>
-            <th>Total Volume</th>
-          </tr>
-        </thead>
-        <tbody>
-          {coins.map((coin) => <TableRow key={coin.id} coin={coin}/>)}
-        </tbody>
-      </table>
+    <div className={styles.container}>
+      {isLoading ? (<RotatingLines strokeColor="#3874ff" strokeWidth="2" /> )
+        : ( <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th>Coin</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>24h</th>
+                  <th>Total Volume</th>
+                </tr>
+              </thead>
+              <tbody>
+                {coins.map((coin) => <TableRow key={coin.id} coin={coin}/>)}
+              </tbody>
+            </table>
+      )}
+      
     </div>
 
   )
@@ -35,14 +43,14 @@ const TableRow = ({coin}) => {
   return (
     <tr>
       <td>
-        <div>
+        <div className={styles.symbol}>
           <img src={image} alt={name} />
           <span>{symbol.toUpperCase()}</span>
         </div>
       </td>
       <td>{name}</td>
       <td>${current_price.toLocaleString()}</td>
-      <td>{price_change.toFixed(2)}%</td>
+      <td className={price_change > 0 ? styles.success : styles.error}>{price_change.toFixed(2)}%</td>
       <td>${total_volume.toLocaleString()}</td>
       <td>{<img src={price_change > 0 ? chartUp : chartDown} alt={name}/>}</td>
     </tr>
--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 
-import TableCoin from "../modules/TableCoin"
-import {getCoinList} from "../../services/cryptoApi"
+// services
+import {getCoinList} from "../../services/cryptoApi";
+
+// component
+import TableCoin from "../modules/TableCoin";
+import Pagination from "../modules/Pagination";
 
 function HomePage() {
 
-   const [coins , setCoins] = useState([])
+   const [coins , setCoins] = useState([]);
+   const [isLoading , setIsLoading] = useState(true);
+   const [page , setPage] = useState(1); 
 
   //  useEffect(() => {
   //   fetch(getCoinList())
@@ -14,16 +20,21 @@ function HomePage() {
   //  } ,[])
 
   useEffect(() => {
+    setIsLoading(true);
     const getData = async () => {
-      const res = await fetch(getCoinList());
+      const res = await fetch(getCoinList(page));
       const json = await res.json();
       setCoins(json);
+      setIsLoading(false);
     }
     getData()
-  } , [])
+  } , [page])
 
   return (
-    <div><TableCoin coins={coins} /></div>
+      <div>
+        <TableCoin coins={coins} isLoading={isLoading}/>
+        <Pagination page={page} setPage={setPage} />
+      </div> 
   )
 }
 
